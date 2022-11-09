2fb6c48f1a5f447abbe554e062edb6e2149ff1db
Christian Bernstein <59013801+christian-bernstein@users.noreply.github.com>
2022-11-09 20:20:25 +0100

    unnamed

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,9 +35,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/christian-bernstein/smarthome-foundation/")
             credentials {
-                // TODO: Always check if this the access token is still valid
-                username = project.findProperty("gpr.user") as String? ?: "christian-bernstein"
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("github-access-token")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
             }
         }
     }
